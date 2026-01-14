c06712584362fd90b10cc870deb840fe66f94b5d
datmaiq <datmaiq@gmail.com>
2026-01-14 18:44:52 +0700

    chore: tracking analyst

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import Quiz from './components/Quiz';
+import { Analytics } from '@vercel/analytics/react';
 
 const App = () => {
-  return <Quiz />;
+  return (
+    <>
+      <Quiz />
+      <Analytics />
+    </>
+  );
 };
 
 export default App;
