bcd388f3ebb386009a46d16a8d2846d5f8318633
datmaiq <datmaiq@gmail.com>
2026-01-14 18:59:50 +0700

    feat: remove unused code

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -5,27 +5,13 @@ export const images = {
   layer2: "/images/layer2.png",
   layer6: "/images/layer6.png",
   
-  // Person images
-  personA: "/images/personA.png",
-  personB: "/images/personB.png",
-  
   // Frame backgrounds
-  backgroundCopy: "/images/backgroundCopy.svg",
-  backgroundCopy1: "/images/backgroundCopy1.png",
+  rectangle3: "/images/rectangle3.svg",
   
   // Card backgrounds  
   group7: "/images/group7.svg",
   rectangle2: "/images/rectangle2.svg",
-  rectangle2Copy: "/images/rectangle2Copy.svg",
-  rectangle2Copy1: "/images/rectangle2Copy1.svg",
-  rectangle2Copy2: "/images/rectangle2Copy2.svg",
-  rectangle2Copy3: "/images/rectangle2Copy3.svg",
-  rectangle2Copy4: "/images/rectangle2Copy4.svg",
-  rectangle2Copy5: "/images/rectangle2Copy5.svg",
-  rectangle2Copy6: "/images/rectangle2Copy6.svg",
-  rectangle3: "/images/rectangle3.svg",
   rectangle4: "/images/rectangle4.svg",
-  rectangle4Copy: "/images/rectangle4Copy.svg",
   rectangle7: "/images/rectangle7.svg",
   rectangle8: "/images/rectangle8.svg",
   rectangle9: "/images/rectangle9.svg",
@@ -34,9 +20,7 @@ export const images = {
   rectangle12: "/images/rectangle12.svg",
   rectangle13: "/images/rectangle13.svg",
   
-  
   // Decorative layers
-  layer4: "/images/layer4.png",
   layer11: "/images/layer11.png",
   layer11Copy: "/images/layer11Copy.png",
   layer12: "/images/layer12.png",
@@ -47,11 +31,4 @@ export const images = {
   layer16Copy: "/images/layer16Copy.png",
   layer16Copy2: "/images/layer16Copy2.png",
   layer17: "/images/layer17.png",
-  
-  // Time/progress indicators
-  time: "/images/time.svg",
-  time1: "/images/time1.svg",
-  time2: "/images/time2.svg",
-  time3: "/images/time3.svg",
-  time4: "/images/time4.svg",
 };
